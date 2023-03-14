905c627aae8bd1ad53d560bbbdd3299dcbcb9a9b
Brady-Malott <brady.malott@gmail.com>
2023-03-13 23:36:58 -0400

    Implemented Acceptance Tests for Archived Posts

--- tests/features/archive-post.feature
+++ tests/features/archive-post.feature
@@ -4,27 +4,32 @@ Feature: Archive Posts
   so i can view the post even if the original post is deleted
 
   Scenario: Successfully archive a post
-    Given that I am on a explore page
-    When I click on the "Archive" link on a post
-    Then the link should change to "Remove from Archive"
-    And a banner shows that the post has been archived
+    Given that I am logged in
+    And I am on the 'Explore' page
+    When I click on the 'Archive' link
+    Then the link should change to 'Remove from Archive'
+    And a banner should show, saying that the post has been 'archived'
 
 
   Scenario: Successfully remove an archived post from the explore page
-    Given that I am on an review page
-    When I click on the "Remove from Archive" link
-    Then the link should change to "Archive"
-    And a banner shows that the post has been removed from my archives
+    Given that I am logged in
+    And I am on the 'Explore' page
+    When I click on the 'Remove from Archive' link
+    Then the link should change to 'Archive'
+    And a banner should show, saying that the post has been 'removed'
 
 
   Scenario: Successfully remove an archived post from the archived page
-    Given that I am on an archived page
-    When I click on the "Remove from Archive" link
+    Given that I am logged in
+    And I am on the 'Profile' page
+    When I click View Archived Posts
+    And I click Remove from Archive
     Then the archived post should disappear
-    And a banner shows that the post has been removed from my archives
+    And a banner should show, saying that the post has been 'removed'
 
 
   Scenario: View archived posts
-    Given that I am on my user profile page
-    When I click on the "View Archived Posts" link
+    Given that I am logged in
+    And I am on the 'Profile' page
+    When I click View Archived Posts
     Then I should be directed to a page displaying all my archived posts
--- tests/features/steps/archive-post-steps.py
+++ tests/features/steps/archive-post-steps.py
@@ -1,52 +1,100 @@
+import os
+from dotenv import load_dotenv
 from behave import given, when, then
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from tests.features.loginHelper import login
+
+basedir = os.path.abspath(os.path.dirname('microblog'))
+load_dotenv(os.path.join(basedir, '.env'))
+
+USERNAME1 = os.getenv('USERNAMEFORTEST')
+PASSWORD1 = os.getenv('PASSWORDFORTEST')
+USERNAME2 = os.getenv('USERNAMEFORTEST2')
+PASSWORD2 = os.getenv('PASSWORDFORTEST2')
+BASETESTURL = os.getenv('SELENIUM_TEST_URL')
 
 
-@given("I am on the todo list page")
+@given('that I am logged in')
 def open_browser(context):
-
     # Implementation of headless from https://stackoverflow.com/questions/46920243/how-to-configure-chromedriver-to-initiate-chrome-browser-in-headless-mode-throug
     # Stackoverflow post desribes what is goin on with options to enable headless chrome
 
     options = Options()
-    options.add_argument("--headless") # Runs Chrome in headless mode.
-    options.add_argument('--no-sandbox') # Bypass OS security model
-    options.add_argument('start-maximized') #to maximize viewport this should still be headless
+    options.add_argument('--headless')  # Runs Chrome in headless mode.
+    options.add_argument('--no-sandbox')  # Bypass OS security model
+    options.add_argument('start-maximized')  # to maximize viewport this should still be headless
     options.add_argument('disable-infobars')
-    options.add_argument("--disable-extensions")
+    options.add_argument('--disable-extensions')
     context.driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
     context.driver.implicitly_wait(5)
-    context.driver.get("http://127.0.0.1:5000/")
+    context.driver.get(BASETESTURL)
+
+    # Login and create an archived post
+    login(context.driver, USERNAME1, PASSWORD1)
+    context.driver.find_element(By.ID, "post").send_keys("Test")
+    context.driver.find_element(By.ID, "submit").click()
+
+    # Switch to another user
+    context.driver.find_element(By.LINK_TEXT, "Logout").click()
+    # context.driver.get(f'{BASETESTURL}/auth/logout')
+    context.driver.get(BASETESTURL)
+    login(context.driver, USERNAME2, PASSWORD2)
 
 
-@when("I enter '{title}' in the title field")
-def step_impl(context, title):
-    title_field = context.driver.find_element(By.NAME, "title")
-    title_field.send_keys(title)
+@given("I am on the '{name}' page")
+def step_impl(context, name):
+    context.driver.find_element(By.LINK_TEXT, name).click()
 
 
-@when("I enter '{estimate}' in the estimate field")
-def step_impl(context, estimate):
-    estimate_field = context.driver.find_element(By.NAME, "estimate")
-    estimate_field.send_keys(estimate)
+@when("I click on the '{link_text}' link")
+def step_impl(context, link_text):
+    clicked_link = context.driver.find_element(By.LINK_TEXT, link_text)
+    # Save the post id (found in the span that is the fourth previous sibling) so we can check the correct link later
+    context.selected_post_id = clicked_link.find_element(By.XPATH, 'preceding-sibling::*[4]').get_dom_attribute('id')
+    clicked_link.click()
 
 
-@when("I click the 'Add' button")
+@when("I click View Archived Posts")
 def step_impl(context):
-    add_button = context.driver.find_element(By.XPATH, "//button[contains(text(),'Add')]")
-    add_button.click()
-    context.driver.implicitly_wait(5)
+    context.driver.find_element(By.LINK_TEXT, 'View Archived Posts').click()
 
 
-@then("the task '{title}' with estimate '{estimate}' should be added to the todo list")
-def step_impl(context, title, estimate):
-    dump_text = context.driver.page_source
-    print(dump_text)
-    assert ("Buy groceries | 2" in dump_text) is True
+# For when the user clicks remove from archive SPECIFICALLY from the archived posts page
+@when("I click Remove from Archive")
+def step_impl(context):
+    clicked_link = context.driver.find_element(By.LINK_TEXT, 'Remove from Archive')
+    # Save the post id (found in the span that is the second previous sibling) so we can check the correct link later
+    context.selected_post_id = clicked_link.find_element(By.XPATH, 'preceding-sibling::*[2]').get_dom_attribute('id')
+    clicked_link.click()
 
 
+# Check that the archived post is no longer there (i.e., cannot find an element with href == context.archive_link_href)
+@then("the archived post should disappear")
+def step_impl(context):
+    assert context.driver.find_elements(By.ID, context.selected_post_id) == []
 
+
+@then("the link should change to '{link_text}'")
+def step_impl(context, link_text):
+    link = context.driver.find_element(By.ID, context.selected_post_id)\
+        .find_element(By.XPATH, 'following-sibling::*[4]')
+    assert link.text == link_text
+
+
+@then("a banner should show, saying that the post has been '{action}'")
+def step_impl(context, action):
+    banner = context.driver.find_element(
+        By.XPATH,
+        f'//html/body/div[@class="container"]/div[@class="alert alert-info"]'
+    )
+    # Check whether the action word ('archived' or 'removed') is in the banner
+    assert action in banner.text
+
+
+@then("I should be directed to a page displaying all my archived posts")
+def step_impl(context):
+    assert 'archived' in context.driver.current_url
