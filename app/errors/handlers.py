593bf16156171f422f19fca77ca629b6e917cca5
zainaraza43 <urboyzain@gmail.com>
2023-04-18 23:00:17 -0400

    handlers done

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,24 +1,31 @@
-from flask import render_template, request
-from app import db
-from app.errors import bp
-from app.api.errors import error_response as api_error_response
-
-
+# A function to determine whether the client wants a JSON response or not
 def wants_json_response():
+    # Check if the request accepts JSON and its weight is higher than HTML
     return request.accept_mimetypes['application/json'] >= \
         request.accept_mimetypes['text/html']
+
+# Error handler for 404 not found errors
 
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
+    # Check if the client wants a JSON response
     if wants_json_response():
+        # Return a JSON response for API errors
         return api_error_response(404)
+    # Otherwise, render a template for the HTML response
     return render_template('errors/404.html'), 404
+
+# Error handler for 500 internal server errors
 
 
 @bp.app_errorhandler(500)
 def internal_error(error):
+    # Rollback the session to ensure data consistency
     db.session.rollback()
+    # Check if the client wants a JSON response
     if wants_json_response():
+        # Return a JSON response for API errors
         return api_error_response(500)
+    # Otherwise, render a template for the HTML response
     return render_template('errors/500.html'), 500
