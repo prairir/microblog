6be238b4792aa0ebef9e330d7bd2b48d4ee5457b
zainaraza43 <urboyzain@gmail.com>
2023-04-18 22:25:16 -0400

    forms done

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,7 +4,9 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
 from app.models import User
 
+
 class LoginForm(FlaskForm):
+    # LoginForm class for the login form.
     username = StringField(_l('Username'), validators=[DataRequired()])
     password = PasswordField(_l('Password'), validators=[DataRequired()])
     remember_me = BooleanField(_l('Remember Me'))
@@ -12,19 +14,24 @@ class LoginForm(FlaskForm):
 
 
 class Confirm2faForm(FlaskForm):
+    # Confirm2faForm class for the two-factor authentication token verification form.
     token = StringField('Token')
     submit = SubmitField('Verify')
 
 
 class Enable2faForm(FlaskForm):
+    # Enable2faForm class for the two-factor authentication enable form.
     verification_phone = StringField('Phone', validators=[DataRequired()])
     submit = SubmitField('Enable 2FA')
 
+
 class Disable2faForm(FlaskForm):
+    # Disable2faForm class for the two-factor authentication disable form.
     submit = SubmitField('Disable 2FA')
 
 
 class RegistrationForm(FlaskForm):
+    # RegistrationForm class for the user registration form.
     username = StringField(_l('Username'), validators=[DataRequired()])
     email = StringField(_l('Email'), validators=[DataRequired(), Email()])
     password = PasswordField(_l('Password'), validators=[DataRequired()])
@@ -34,22 +41,26 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField(_l('Register'))
 
     def validate_username(self, username):
+        # Validates that the username is not already taken.
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError(_('Please use a different username.'))
 
     def validate_email(self, email):
+        # Validates that the email is not already in use.
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError(_('Please use a different email address.'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
+    # ResetPasswordRequestForm class for the password reset request form.
     email = StringField(_l('Email'), validators=[DataRequired(), Email()])
     submit = SubmitField(_l('Request Password Reset'))
 
 
 class ResetPasswordForm(FlaskForm):
+    # ResetPasswordForm class for the password reset form.
     password = PasswordField(_l('Password'), validators=[DataRequired()])
     password2 = PasswordField(
         _l('Repeat Password'), validators=[DataRequired(),
