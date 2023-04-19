3bb4bffdc52108a8636b04e562d1994c7d4760d3
zainaraza43 <urboyzain@gmail.com>
2023-04-18 22:22:37 -0400

    emails done

--- app/auth/email.py
+++ app/auth/email.py
@@ -2,9 +2,15 @@ from flask import render_template, current_app
 from flask_babel import _
 from app.email import send_email
 
+# Sends a password reset email to a user containing a token to reset their password.
+
 
 def send_password_reset_email(user):
+    # Generate a token for resetting the user's password.
     token = user.get_reset_password_token()
+
+    # Use the `send_email` function from the `email` module to send the reset password email to the user.
+    # The email includes a subject, sender, recipients, and both text and HTML versions of the email body.
     send_email(_('[Microblog] Reset Your Password'),
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
